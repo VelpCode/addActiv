1e27c098fe71984358cb52f446c6ba50381a574f
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-08 18:21:27 -0500

    Connecting DB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express();
-
+require("./conn/conn")
 
 app.get("/", (req,res) => {
     res.send("Hello Server.")
---
+++ backend/conn/conn.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const conn = async (req,res) => {
+    
+    try {
+        await mongoose
+        .connect(
+        "mongodb+srv://velpmail:skrishna1@cluster0.dsx8qnt.mongodb.net/"
+        ).then(() => {
+            console.log("DB Connected.")
+        })
+    } catch (error) {
+        res.status(400).json({
+            message: "Not Connected",
+        })
+    }
+    
+};
+conn();
