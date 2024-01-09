8d59b7facf324d52e5eb0d9dcff43cc2c98444a7
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-09 14:10:19 -0500

    updateGetmethod

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -56,8 +56,13 @@ router.put("/updateTask/:id", async (req,res) => {
 
  //getTask
  router.get("/getTasks/:id",async( req, res ) => {
-    const list = await List.find({ user:req.params.id });
-    res.status(200).json({ list });
+    const list = await List.find({ user:req.params.id }).sort({ createdAt: -1 });
+    if (list.length !== 0) {
+        res.status(200).json({ list: list });
+    } else {
+        res.status(200).json({ message: "No tasks left" });
+    }
+    
  }
  )
 
