f69f0a3ea4eae240c150c059ae262329b07edc28
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-09 16:00:54 -0500

    Setting up Frontend Home method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   )
 }
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
+import "./Navbar.css"
+import { GiBookCover } from "react-icons/gi";
+
 
 const Navbar = () => {
   return (
-    <div><nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#"><b>Activity Add</b></a>
+    <div><nav className="navbar navbar-expand-lg">
+    <div className="container">
+      <a className="navbar-brand" href="#"><b><GiBookCover/> &nbsp;∀ctivities</b></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item">
+          <li className="nav-item mx-2">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item mx-2">
             <a className="nav-link active" aria-current="page" href="#">About Us</a>
         </li>
-        <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Sign Up</a>
+        <li className="nav-item mx-2">
+            <a className="nav-link active btn-nav" aria-current="page" href="#">Sign Up</a>
         </li>
-        <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Sign In</a>
+        <li className="nav-item mx-2">
+            <a className="nav-link active btn-nav" aria-current="page" href="#">Sign In</a>
         </li>
-        <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">LogOut</a>
+        <li className="nav-item mx-2">
+            <a className="nav-link active btn-nav" aria-current="page" href="#">Log Out</a>
         </li>
         <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">
-                <img src = "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"></img>
+                <img className = "img-fluid user-png"
+                src = "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"></img>
             </a>
         </li>
         </ul>
