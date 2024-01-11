a76321a69569a2634d3b5d93e7aa22a0535ae49f
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-11 13:44:19 -0500

    making singup page

--- frontend/src/components/signup/Signup.jsx
+++ frontend/src/components/signup/Signup.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import "./signup.css"
+import Logo from "./dudedraw.png";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+
+    <div className="signup d-flex justify-content-center align-items-center">
+        <div className="signblock">
+            <div className='block-left'>
+                <h1>Sign Up Now</h1><br></br>
+                <div className="infodetails">
+                <input type="text" className="text" placeholder='Enter your email' />
+            </div>
+            <div className="passdetails">
+                <input type="text" className="text" placeholder='Enter your password'/>
+                </div>
+                <button class = "signbtn">Sign Up</button>
+            </div>
+
+            <div className="block-right">
+                <img className = "imagebro "src = {Logo} alt = ""></img>
+            </div>
+
+        </div>
+    </div>
+
   )
 }
 
