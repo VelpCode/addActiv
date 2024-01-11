a3cb491e01be47c0b136b16509687f56f5ba087f
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-11 12:47:16 -0500

    homepage complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Home from './components/home/Home';
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   )
 }
--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './footer.css';
+
 
 const Footer = () => {
   return (
-    <div className="container-fluid">Activity List<p>&copy;</p></div>
+    <div className="container-fluid text-center d-flex justify-content-center align-items-center footer">
+      <h4>∀ctivities</h4> &nbsp;
+      <p>&copy;</p>
+      <h6 classname = "velptext">VELP.</h6>
+    </div>
   )
 }
 
