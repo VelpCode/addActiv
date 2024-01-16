7e2d2bffb5b5f77da7645ac539c894afc5c77fea
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-15 19:14:21 -0500

    REQ failing

--- frontend/src/components/signin/Signin.jsx
+++ frontend/src/components/signin/Signin.jsx
@@ -7,7 +7,6 @@ const Signin = () => {
 
     const [Inputs, setInputs] = useState({
         email: "",
-        username: "",
         password: "",
     });
     const change = (e) => {
@@ -16,37 +15,34 @@ const Signin = () => {
     };
     const submit = async (e) => {
         e.preventDefault();
-        await axios.post("http://localhost:1000/api/v1/signin", Inputs)
+        await axios
+        .post("http://localhost:1000/api/v1/signin", Inputs)
         .then((response) => {
-            console.log(alert(response.data.message));
+            console.log(alert(response.data));
         });
     }
 
   return (
 
     <div className="signin d-flex justify-content-center align-items-center">
-        <div className="signblock">
-            <div className='block-left'>
-                <h1>Sign In</h1><br></br>
-                <div className="infodetails">
-                <input type="email" name = "email" className="text" placeholder='Enter your email' 
-                value={Inputs.email}
-                onChange={change}/>
+    <div className="signblock">
+        <div className='block-left'>
+            <h1>Sign In</h1><br></br>
+            <div className="infodetails">
+                <input type="email" name="email" className="text" placeholder='Enter your email' onChange={change} value={Inputs.email}/>
             </div>
             <div className="passdetails">
-                <input type="password" name = "password" className="text" placeholder='Enter your password'
-                value={Inputs.password}
-                onChange={change}/>
-                </div>
-                <button class = "signbtn" onClick={submit}>Sign In</button>
+                <input type="password" name="password" className="text" placeholder='Enter your password' onChange={change} value={Inputs.password}/>
             </div>
+            <button className="signbtn" onClick={submit}>Sign In</button>
+        </div>
 
-            <div className="block-right">
-                <img className = "imagebro "src = {Logo} alt = ""></img>
-            </div>
-
+        <div className="block-right">
+            <img className="imagebro" src={Logo} alt=""></img>
         </div>
     </div>
+</div>
+
 
   )
 }
