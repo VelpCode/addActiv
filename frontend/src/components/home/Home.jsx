45e9a8a45c625c3b8922b1b368c9b39ed94bfaa0
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-23 14:51:20 -0500

    AXIOS DEBUGGING HELP

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import "./home.css"
 import { IoAddCircleOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { ReactComponent as YourIcon } from './undraw_fun-underline.svg';
+import { ReactComponent as Sport } from './sport.svg';
+import { ReactComponent as Blocks } from './blocks.svg';
+
+
 
 const Home = () => {
   return (
@@ -11,6 +16,9 @@ const Home = () => {
             <p>Elimate unnessary activies, stay on top of your goals and tasks<br></br>
             with the best activity tracker on the market    .</p>
             <Link to="/active" className="dudebutton">Add Activity <IoAddCircleOutline size={20} className="icon-vertical-adjust"/></Link>
+            <YourIcon  className = "mamooty"/>
+            <Sport className = "sport"/>
+            <Blocks className = "block"/>
         </div>
     </div>
   )
